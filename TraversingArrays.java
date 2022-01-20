12b0d11b655f7c3eef9bb545e8f865c11677721b
brady butcher <87883036+bradybutcher@users.noreply.github.com>
2022-01-19 23:22:26 -0500

    Create TraversingArrays.java

---
+++ TraversingArrays.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class TraversingArrays {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements in the array: ");
+        int number = Integer.parseInt(scan.nextLine());
+
+        int[] store = new int[number];
+        int[] occurrences = new int[26];
+        int min = 0;
+        int max = 25;
+        int randNum;
+
+        for (int i = 0; i < store.length; i++) { // generates random number
+            randNum = min + (int) (Math.random() * (max - min));
+
+            if (randNum < max) {
+                randNum = min + (int) (Math.random() * (max - min));
+                ;
+            }
+
+            store[i] = randNum;
+        }
+
+        for (int i = 0; i < occurrences.length; i++) {
+            occurrences[i]++;
+        }
+
+        for (int i = 0; i < occurrences.length; i++) {
+            System.out.println(i + " " + occurrences[i]);
+        }
+    }
+}
