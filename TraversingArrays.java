6968e657e5521a41d79de1fc4db317ca4c8eac40
brady butcher <87883036+bradybutcher@users.noreply.github.com>
2022-01-20 13:08:10 -0500

    Update TraversingArrays.java

--- TraversingArrays.java
+++ TraversingArrays.java
@@ -12,22 +12,29 @@ public class TraversingArrays {
         int min = 0;
         int max = 25;
         int randNum;
+        int temp = 0;
 
         for (int i = 0; i < store.length; i++) { // generates random number
             randNum = min + (int) (Math.random() * (max - min));
 
             if (randNum < max) {
                 randNum = min + (int) (Math.random() * (max - min));
-                ;
             }
 
             store[i] = randNum;
         }
 
         for (int i = 0; i < occurrences.length; i++) {
-            occurrences[i]++;
+            temp = store[i];
+            occurrences[temp]++;
         }
 
+        System.out.println("Generated Numbers: ");
+        for (int i = 0; i < store.length; i++) {
+            System.out.println(store[i]);
+        }
+
+        System.out.println("Occurrences: ");
         for (int i = 0; i < occurrences.length; i++) {
             System.out.println(i + " " + occurrences[i]);
         }
